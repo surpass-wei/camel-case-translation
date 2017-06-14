991191bfaf59105d307e347dfbd2faf6c1353786
cwei <cwei@cz2r.com>
2017-06-14 17:46:37 +0800

    完整点击翻译：支持驼峰、下划线，中划线

--- popup.js
+++ popup.js
@@ -4,30 +4,73 @@ var config = {
     key: '14rKDZO4EY0bR2a1yD4FaVRLuSBR1RBR'
 }
 
-var addEvent = function(obj, type, handle) {
-    try {  // Chrome、FireFox、Opera、Safari、IE9.0及其以上版本
-        obj.addEventListener(type, handle, false);
-    } catch (e) {
-        try {  // IE8.0及其以下版本
-            obj.attachEvent('on' + type, handle);
-        } catch (e) {  // 早期浏览器
-            obj['on' + type] = handle;
+var requiredParam = {
+    q: "mark",
+    from: "auto",
+    to: "auto"
+}
+
+//  获取requiredParam构建好请求的URL，调用doGet
+var getTranslateResult = function () {
+    //  生成签名
+    var salt = parseInt(Math.random() * 100);
+    var signStr = config.appKey + requiredParam.q + salt + config.key;
+    var sign = md5(signStr);
+
+    var requestUrl = config.apiUrl + "?" + "q=" + requiredParam.q + "&from=" + requiredParam.from + "&to=" + requiredParam.to + "&appKey=" + config.appKey + "&salt=" + salt + "&sign=" + sign;
+    var requestUrlEnc = encodeURI(requestUrl);
+    doGet(requestUrlEnc, function (translateResult) {
+        var resultJson = JSON.parse(translateResult);
+        document.getElementById('resultDiv').innerHTML = resultJson.translation;
+    });
+}
+
+//  发起请求
+var doGet = function (theUrl, callback) {
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.onload = function (e) {
+        if (this.status == 200 || this.status == 304) {
+            callback(this.responseText);
         }
+    }
+    xmlHttp.send();
+}
+
+//  点击翻译
+var byClick = function () {
+    var theQ = document.getElementById('content').value;
+    requiredParam.q = convertToBase(theQ);
+    requiredParam.from = document.getElementById('whatFrom').value;
+    requiredParam.to = document.getElementById('whatTo').value;
+
+    //  当未输入内容时，直接返回
+    if (requiredParam.q.replace(/(^s*)|(s*$)/g, "").length == 0) {
+        document.getElementById('resultDiv').innerHTML = "";
+        return;
+    }
+
+    getTranslateResult();
+}
+
+//  对驼峰、下划线、中划线等特殊格式进行转换
+var convertToBase = function (oldStr) {
+    //  驼峰转换
+    if (/^([a-z]|[A-Z])+([A-Z][a-z]+)$/.test(oldStr)) {
+        return oldStr.replace(/([A-Z])/g, " $1").toLowerCase();  //  驼峰转化
+    } else if (/^([a-z]|[A-Z])+([-][a-z]+)+$/.test(oldStr)) {
+        return oldStr.replace(/(-)([a-zA-Z])/g, " $2").toLowerCase() //  中划线
+    } else if (/^([a-z]|[A-Z])+([_][a-z]+)+$/.test(oldStr)) {
+        return oldStr.replace(/(_)([a-zA-Z])/g, " $2").toLowerCase() //  下划线
+    } else {
+        return oldStr;
     }
 }
 
-var getTranslateResult = function(q, from, to) {
-    //  生成签名
-    var salt = parseInt(Math.random() * 100);
-    var signStr = config.appKey + q + salt + config.key;
-    var sign = md5(signStr);
-
-    var resultUrl = config.apiUrl+"?"+"q="+q+"&from="+from+"&to="+to+"&appKey="+config.appKey+"&salt="+salt+"&sign="+sign;
-    var result = encodeURI(resultUrl);
-
-    var request = new XMLHttpRequest();
-    request.open("GET",result);
-    request.send();
+//  取词翻译
+var bySelect = function () {
+    alert("取词翻译，尚未完成");
 }
 
-//addEvent(document.getElementById("btn"), "click", getTranslateResult('hello world', 'EN', 'zh-CHS'));
+document.getElementById('translateBtn').onclick = byClick;
